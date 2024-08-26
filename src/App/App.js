970426bb12aec2faa403824180877eb6d7b7cdd0
Kellbis <holasoykellbis@gmail.com>
2024-08-26 13:01:15 -0400

    composition and structure example

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,71 @@
 import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
+// import { TodoProvider } from '../TodoContext';
+// import { AppUI } from './AppUI';
 
 function App() {
+	const [state, setState] = React.useState('ratatat');
 	return (
-		<TodoProvider>
-			<AppUI />
-		</TodoProvider>
+		<React.Fragment>
+			<TodoForDo props={'To do - For do'} />
+			<TodoSystem state={state} />
+		</React.Fragment>
 	);
 }
+function TodoForDo({ props }) {
+	return (
+		<React.Fragment>
+			<h2>{props}</h2>
+		</React.Fragment>
+	);
+}
+function TodoSystem({ state }) {
+	return (
+		<React.Fragment>
+			<TodoInfoAndSearch>
+				<NumbersTodo />
+				<TodoSearch />
+			</TodoInfoAndSearch>
+			<TodoList>
+				<TodoItem state={state} />
+			</TodoList>
+		</React.Fragment>
+	);
+}
+
+function TodoInfoAndSearch({ children }) {
+	return <div>{children}</div>;
+}
+
+function NumbersTodo() {
+	return (
+		<React.Fragment>
+			<p>NumbersTodo</p>
+		</React.Fragment>
+	);
+}
+
+function TodoSearch() {
+	return (
+		<React.Fragment>
+			<p>TodoSearch</p>:
+		</React.Fragment>
+	);
+}
+
+function TodoList({ children }) {
+	return <ul className="TodoList"> {children}</ul>;
+}
+
+function TodoItem({ state }) {
+	return <p>Todoitemrch: {state}</p>;
+}
+
+// function App() {
+// 	return (
+// 		<TodoProvider>
+// 			<AppUI />
+// 		</TodoProvider>
+// 	);
+// }
 
 export default App;
